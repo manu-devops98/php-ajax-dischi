1af1e495bdfee80ffb9feee1deb26d4956c59f57
manu-devops98 <manuelcappello7@gmail.com>
2022-02-02 15:20:47 +0100

    add milestone 1 con aggiunta dischi in php

--- index-php.php
+++ index-php.php
@@ -11,6 +11,21 @@
     <!-- HEADER      -->
     <?php include __DIR__ . '/partials/header.php' ?>
     <!-- MAIN  -->
+    <main>
+        <?php include_once __DIR__ . '/server/db.php' ?>
+    <div class="container-cards">
+        <?php foreach ($cards as $card) { ?>
+        <div class="cards">
+            <div class="container-img">
+               <img src="<?= $card['poster'] ?>" alt="">
+            </div>
+            <h1><?= $card['title'] ?></h1>
+            <h3><?= $card['author'] ?></h3>
+            <h4><?= $card['year'] ?></h4>
+        </div>
+        <?php } ?>
+    </div>
+    </main>
     <!-- FOOTER  -->
 </body>
 </html>
